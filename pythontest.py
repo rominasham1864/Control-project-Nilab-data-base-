e48e5ab2f715dfa8b0641db2d7f077278a7151e6
romina <123378481+rominasham1864@users.noreply.github.com>
2023-07-24 13:08:15 +0330

    working on table
    product, available_in_stock, ref_date, o_date, req_n, Qty are done but still can't add comments on excel file some columns should be added. no idea on secured cells. getting table name should be input or find a connection. for now we are just working on Alteymour project.

--- pythontest.py
+++ pythontest.py
@@ -1,23 +1,37 @@
 import pandas as pd
 import pymysql
 import openpyxl
+import os
 
-workbook = openpyxl.load_workbook('REM-40204-777-294(06).xlsm', keep_vba=True)
-
-worksheet = workbook['REM-40204-777-294']
-
-cell_value = worksheet['W4'].value
-
-conn = pymysql.connect(host="localhost", user="root", password="1122", database="request_control")
-
-
+workbook = openpyxl.load_workbook(
+    "REM-40204-777-294(06).xlsm", keep_vba=True, data_only=True
+)
+conn = pymysql.connect(
+    host="localhost", user="root", password="1122", database="request_control"
+)
 cur = conn.cursor()
 
-cur.execute("CREATE TABLE IF NOT EXISTS mytable (value VARCHAR(255))")
 
-cur.execute("INSERT INTO mytable VALUES (%s)", (cell_value,))
+worksheet = workbook["REM-40204-777-294"]
+
+req_n = os.path.basename(
+    "C:/Users/alire/Desktop/rominas workspace/REM-40204-777-294(06).xlsm"
+)
+
+o_date = worksheet.cell(row=6, column=14).value
+ref_date = worksheet.cell(row=17, column=14).value
+
+for row in range(8, 14):
+    prod = worksheet.cell(row=row, column=3).value
+    qty = worksheet.cell(row=row, column=9).value
+    available = worksheet.cell(row=row, column=11).value
+    print(qty)
+    if prod != None:
+        sql = "INSERT INTO alteymour (product, req_n, Qty, o_date, available_in_stock, ref_date) VALUES (%s, %s, %s,%s,%s, %s)"
+        val = (prod, req_n, qty, o_date, available, ref_date)
+        cur.execute(sql, val)
+        
+           
 
 conn.commit()
-
-# Close the cursor and the connection to the MySQL database
 cur.close()
